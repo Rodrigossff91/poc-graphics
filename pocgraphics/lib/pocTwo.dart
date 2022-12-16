61a52db3b82a890100815827f250f2af9eee6850
Rodrigo Ferreira <43223244+Rodrigossff91@users.noreply.github.com>
2022-12-16 17:00:31 -0300

    Graficos Logar

---
+++ pocgraphics/lib/pocTwo.dart
@@ -0,0 +1,108 @@
+/// Flutter package imports
+import 'package:flutter/material.dart';
+import 'package:pocgraphics/category.dart';
+import 'package:pocgraphics/default.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+/// Local imports
+
+/// Renders the linear gauge sleep watch showcase sample.
+class SleepWatch extends StatefulWidget {
+  const SleepWatch({
+    super.key,
+  });
+
+  /// Creates the linear gauge sleep watch showcase sample.
+
+  @override
+  State<SleepWatch> createState() => _SleepWatchState();
+}
+
+class _SleepWatchState extends State<SleepWatch> {
+  TooltipBehavior? _tooltipBehavior;
+
+  @override
+  void initState() {
+    _tooltipBehavior =
+        TooltipBehavior(enable: true, header: '', canShowMarker: false);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            //  const Far(),
+            CategoryDefault(),
+            DefaultPanning()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChartSampleData {
+  /// Holds the datapoint values like x, y, etc.,
+  ChartSampleData(
+      {this.x,
+      this.y,
+      this.xValue,
+      this.yValue,
+      this.secondSeriesYValue,
+      this.thirdSeriesYValue,
+      this.pointColor,
+      this.size,
+      this.text,
+      this.open,
+      this.close,
+      this.low,
+      this.high,
+      this.volume});
+
+  /// Holds x value of the datapoint
+  final dynamic x;
+
+  /// Holds y value of the datapoint
+  final num? y;
+
+  /// Holds x value of the datapoint
+  final dynamic xValue;
+
+  /// Holds y value of the datapoint
+  final num? yValue;
+
+  /// Holds y value of the datapoint(for 2nd series)
+  final num? secondSeriesYValue;
+
+  /// Holds y value of the datapoint(for 3nd series)
+  final num? thirdSeriesYValue;
+
+  /// Holds point color of the datapoint
+  final Color? pointColor;
+
+  /// Holds size of the datapoint
+  final num? size;
+
+  /// Holds datalabel/text value mapper of the datapoint
+  final String? text;
+
+  /// Holds open value of the datapoint
+  final num? open;
+
+  /// Holds close value of the datapoint
+  final num? close;
+
+  /// Holds low value of the datapoint
+  final num? low;
+
+  /// Holds high value of the datapoint
+  final num? high;
+
+  /// Holds open value of the datapoint
+  final num? volume;
+}
